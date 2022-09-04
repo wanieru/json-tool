1f4d6289e8b1beca4c0f3b782269746a322feef8
wanieru <github@wanieru.com>
2022-09-04 22:11:24 +0200

    Move main.js to lib/json-tool.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ const path = require('path');
 module.exports = {
     entry: './dist/index.js',
     output: {
-        filename: 'main.js',
-        path: __dirname + "/www/js",
+        filename: 'json-tool.js',
+        path: __dirname + "/lib",
     },
     resolve: {
     },
