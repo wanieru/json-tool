3f607d4c0f78abfb3ba8fa67bb500fa5397d1b8a
wanieru <github@wanieru.com>
2022-11-28 22:41:37 +0100

    Add a docs page

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,9 @@ module.exports = {
     entry: {
         'lib/json-tool.js': './js/index.js',
         'www/js/json-tool.js': './js/index.js',
-        'www/js/main.js': './js/www.js'
+        'www/js/main.js': './js/www.js',
+        'docs/js/json-tool.js': './js/index.js',
+        'docs/js/main.js': './js/www.js'
     },
     output: {
         filename: '[name]',
